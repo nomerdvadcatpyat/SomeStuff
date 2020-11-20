5b9c622018e9e6bb8639df8884427d27a2f3b81e
k <nomerdvadcatpyat@gmail.com>
2020-11-20 16:33:49 +0500

    mdn end, without 7ch

--- web/sol/express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ web/sol/express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 var BookInstance = require('../models/bookinstance');
+const { body,validationResult } = require('express-validator');
+var Book = require('../models/book');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res) {
@@ -12,13 +14,29 @@ exports.bookinstance_list = function(req, res) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
 };
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+  Book.find({},'title')
+  .exec(function (err, books) {
+    if (err) { return next(err); }
+    // Successful, so render.
+    res.render('bookinstance_form', {title: 'Create BookInstance', book_list: books});
+  });
 };
 
 // Handle BookInstance create on POST.
